0f31d7d169e34178960b2e3b013fd5a63900f51d
Saranhgg <87320099+Saranhgg@users.noreply.github.com>
2021-09-28 14:52:30 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -86,7 +86,7 @@ async def auto_filter(bot, update):
                 file_GB = f"[{str(round((file_size/(1024*1024)),2))} GB]"
                 size = file_GB
                 
-            file_name = size + " 🎥 " + file_name
+            file_name = " 🎬 " + file_name + " 🎪 " + size 
             
             print(file_name)
             #file_size = str(file_size) + " KB" if file_size < 1024 elif file_size < 1024 else str(round(file_size/1024)) + " GiB"  #"📁 " + 
